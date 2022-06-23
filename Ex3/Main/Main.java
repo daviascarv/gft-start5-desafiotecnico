812032a843582d809479d2ff43889c06acd02cc1
Davi Augusto <davi_augusto11@hotmail.com>
2022-06-23 17:25:42 -0300

    Exercicio 3 iniciado

---
+++ Ex3/Main/Main.java
@@ -0,0 +1,14 @@
+package TesteGFT.Ex3.Main;
+
+import TesteGFT.Ex3.Pessoa;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Pessoa ps = new Pessoa("Cristiano Ronaldo", "Portugal", "848412265654");
+
+
+        System.out.println(ps);
+    }
+    
+}
